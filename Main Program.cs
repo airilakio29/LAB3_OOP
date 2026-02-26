24cabc1315797f60c8076751734d6b67f7c096d9
airilakio29 <airilasyraff06@gmail.com>
2026-02-26 17:53:24 +0800

    Main Program
    the output an user will see

---
+++ Main Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        InputHandler input = new InputHandler();
+        MatrixCalculator matrixCalc = new MatrixCalculator();
+        VectorCalculator vectorCalc = new VectorCalculator();
+
+        // MATRIX ADDITION
+        Console.WriteLine("Matrix A:");
+        Matrix A = input.inputMatrix();
+
+        Console.WriteLine("Matrix B:");
+        Matrix B = input.inputMatrix();
+
+        Matrix resultMatrix = matrixCalc.add(A, B);
+
+        Console.WriteLine("Matrix Addition Result:");
+        resultMatrix.display();
+
+        // VECTOR ADDITION
+        Console.WriteLine("Vector A:");
+        Vector vA = input.inputVector();
+
+        Console.WriteLine("Vector B:");
+        Vector vB = input.inputVector();
+
+        Vector resultVector = vectorCalc.add(vA, vB);
+
+        Console.WriteLine("Vector Addition Result:");
+        resultVector.display();
+    }public class VectorCalculator
+{
+    private DimensionValidator validator;
+
+    public VectorCalculator()
+    {
+        validator = new DimensionValidator();
+    }
+
+    public Vector add(Vector A, Vector B)
+    {
+        if (!validator.validateVectorAddition(A, B))
+            throw new Exception("Vector dimension are not compatible");
+
+        Vector result = new Vector(A.getSize());
+
+        for (int i = 0; i < A.getSize(); i++)
+        {
+            result.setValue(i,
+                A.getValue(i) + B.getValue(i));
+        }
+
+        return result;
+    }
+}
+
+}
